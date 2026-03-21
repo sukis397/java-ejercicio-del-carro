962b26ffda7b9388d8d4550c2fb42a112481cd3d
dilanstevenms-maker <dilanstevenms@gmail.com>
2026-03-20 19:50:35 -0500

    Agregados métodos get y set en la clase Banco

--- Persona.java
+++ Persona.java
@@ -1,15 +1,9 @@
 package banco;
-/**
- * La clase Persona representa a una persona que
-posee una o más cuentas dentro del banco.
- */
+
 public class Persona {
-	//Atributo de instancia
+
 	private String nombre;
-	/**
-	 * Constructor 
-	 * @param nombre de laPersona
-	 */
+
 	public Persona(String nombre) {
 		super();
 		this.nombre = nombre;
